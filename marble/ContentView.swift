bbb934bab88a67268ba41a7d98c6368e6450f1b0
enzo-prism <215033440+enzo-prism@users.noreply.github.com>
2026-01-12 07:32:53 -0800

    Initial Commit

---
+++ marble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  marble
+//
+//  Created by Enzo on 1/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ marble/marbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  marbleApp.swift
+//  marble
+//
+//  Created by Enzo on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct marbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
